c4d31db8e0d104d98ec4e465862c420d308c501b
Aleksandar Popovic <aleksandar.popovic@gmail.com>
2022-07-31 21:15:49 +0200

    Initial commit

---
+++ spec/bugsify_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Bugsify do
+  it "has a version number" do
+    expect(Bugsify::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
