db993ca23a9f507ae70f1e4d7a2427437396dd08
aleksandar-popovic <aleksandar.popovic@hotmail.com>
2025-05-21 12:06:19 +0200

    fix gen

--- lib/bugsonar/client/client.rb
+++ lib/bugsonar/client/client.rb
@@ -8,11 +8,11 @@ module Bugsonar
   module Client
     class Api
       def request(method, body = nil)
-        api_url = ENV["BUGSONAR_DEV_MODE"] == "TRUE" ? "http://localhost:3000" : "https://api.bugsonar.com"
+        api_url = ENV.has_key?("RAILS_ENV") && ENV["RAILS_ENV"] == "development" ? "http://localhost:3000" : "https://api.bugsonar.com"
 
         uri = URI.parse("#{api_url}/v2/collectors/ruby")
         http = Net::HTTP.new(uri.host, uri.port)
-        http.use_ssl = ENV["BUGSONAR_DEV_MODE"] == "TRUE" ? false : true
+        http.use_ssl = ENV.has_key?("RAILS_ENV") && ENV["RAILS_ENV"] == "production"
 
         klass = "Net::HTTP::#{method}"
         constantized = Object.const_get(klass)
