608dca8a36d85d79970aaf4d88db06ef2d5ce778
Aleksandar Popovic <aleksandar.popovic@hotmail.com>
2025-05-24 23:01:46 +0200

    Update environment.rb

--- lib/bugsonar/environment/environment.rb
+++ lib/bugsonar/environment/environment.rb
@@ -1,5 +1,15 @@
 # frozen_string_literal: true
 
+# Development Environment:
+
+This is where developers work, writing and testing code, often using their local machines or a shared development environment. It's a controlled space where developers can make changes without affecting other parts of the system.
+Testing/QA Environment:
+This environment is dedicated to rigorously testing the software to ensure it meets requirements and functions correctly. Testers perform various tests, including unit, integration, and regression testing, to identify and fix bugs before deployment.
+Staging Environment:
+This environment is designed to closely resemble the production environment. It's used for final testing, including user acceptance testing (UAT), and ensures that the software behaves as expected under real-world conditions before it's released to production.
+Production Environment:
+This is the live environment where end-users interact with the software. It's the most stable and secure environment, and deployments are carefully planned to minimize downtime. 
+
 module Bugsonar
   module Environment
     def fetch_environment
